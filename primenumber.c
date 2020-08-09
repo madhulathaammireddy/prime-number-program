b692631f0973d92666ba04ad45d60e4dd60c6051
madhulathaammireddy <69135884+madhulathaammireddy@users.noreply.github.com>
2020-08-09 17:52:57 +0530

    Created by Madhu latha
    This code will display whether the given number is prime or not

---
+++ primenumber.c
@@ -0,0 +1,37 @@
+\\ write a c program to find whether given number is prime or not using recursion
+#include<studio.h>
+int primeno(int,int);
+int main()
+{
+int num,check;
+printf("enter a number:");
+scanf("%d"&num);
+check=primeno(num,num/2);
+if(check==1)
+{
+printf("%d is a prime number\n",num);
+}
+else
+{
+printf("%d is not a prime number\n",num);
+}
+return 0;
+}
+int primeno(int num,int i)
+{
+if(i==1)
+{
+return 1;
+}
+else
+{
+if(num%i==0)
+{
+return 0;
+}
+else
+{
+return primeno(num,i-1);
+}
+}
+}
